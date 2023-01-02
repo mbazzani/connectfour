898feefd9955c469efc12a9212f0cf1239ae9621
mbazzani <114893351+mbazzani@users.noreply.github.com>
2023-01-01 22:33:23 -0500

    Initial commit, minimum viable game against minimax algorithm

---
+++ connectfour.py
@@ -0,0 +1,194 @@
+import numpy as np
+from scipy.signal import convolve2d
+import math
+import random
+ROWS=6
+COLUMNS=7
+
+def print_board(board):
+    icons = {0:'_',1:'O',2:'X'}
+    for row in board:
+        for val in row:
+            print(icons[val], end=' ')
+        print()
+
+    for i in range(1,COLUMNS+1):
+        print(i, end=' ')
+    print(end='\n\n')
+
+def valid_move(board, move):
+    if move<0 or move>=COLUMNS:
+        return False 
+    if board[0, move]!=0:
+        return False
+    return True
+
+def make_move(board, move, player):
+    for i in range(ROWS-1,-1,-1):
+        if board[i, move]==0:
+            board[i, move]=player
+            return
+
+def unmake_move(board, move):
+    for i in range(ROWS):
+        if board[i, move]!=0:
+            board[i, move]=0
+            return
+
+def static_eval(board):
+    return random.random()
+
+def one_hot(board, player):
+    return np.asarray(np.isin(board, player), dtype=np.uint8)
+
+def check_winning(board, player):
+    new_board = one_hot(board, player)
+    horizontal_kernel = np.array([[1, 1, 1, 1]])
+    vertical_kernel = np.transpose(horizontal_kernel)
+    diag1_kernel = np.eye(4, dtype=np.uint8)
+    diag2_kernel = np.fliplr(diag1_kernel)
+    detection_kernels = [horizontal_kernel, 
+                         vertical_kernel, 
+                         diag1_kernel, 
+                         diag2_kernel]
+
+    for kernel in detection_kernels:
+        if (convolve2d(new_board, kernel, mode='valid') == 4).any():
+            return True
+    return False
+
+def minimax(board, depth, alpha, beta, player, previous_move):
+
+    other_player = int(not bool(player-1))+1
+    #print("Player: " + str(player))
+    #print("Other:  " + str(other_player))
+    if check_winning(board, 1):
+        return 100+depth
+    if check_winning(board, 2):
+        return -(100+depth)
+    elif depth==0:
+        return static_eval(board)
+
+    # Player 1 finds highest value move
+    if player==1:
+        max_move_eval = -math.inf
+        for move in range(COLUMNS):
+            if valid_move(board, move):
+                make_move(board, move, player)
+                move_eval = minimax(board, depth-1, alpha, beta, 2, move)
+                unmake_move(board, move)
+                max_move_eval = max(max_move_eval, move_eval)
+                alpha = max(alpha, move_eval)
+                if beta <= alpha:
+                    break
+        return max_move_eval
+
+    # Player 2 finds lowest (for player one) value move
+    else:
+        min_move_eval = math.inf
+        for move in range(COLUMNS):
+            if valid_move(board, move):
+                #print_board(board)
+                make_move(board, move, player)
+                move_eval = minimax(board, depth-1, alpha, beta, 1, move)
+                unmake_move(board, move)
+                min_move_eval = min(min_move_eval, move_eval)
+                beta = min(beta, move_eval)
+                if beta <= alpha:
+                    break
+        return min_move_eval
+
+def prompt(message, validity_test, response_type=None):
+    response=''
+    while True:
+        try:
+            print(message)
+            if response_type is None:
+                response = input()
+            else:
+                response = response_type(input())
+        except ValueError:
+            print('Please enter a valid value')
+            continue
+        if validity_test(response):
+            return response
+        else:
+            print('Please enter a valid value')
+            continue
+    return response
+
+
+def prompt_end():
+    yes_responses = ('Y','Ye','Yes','yes','ye','y')
+    no_responses = ('N', 'No', 'no', 'n')
+    response = prompt('\nWould you like to play another game? [y/n]',
+                      lambda x: x in yes_responses+no_responses)
+    if response in yes_responses:
+        return False
+    else:
+        return True
+
+
+def player_move(board, player):
+    move = prompt('What move will you make?',
+                  lambda x: valid_move(board, x-1),
+                  response_type=int)
+    move-=1
+    make_move(board, move, player)
+    print('Here is the result of your move: ')
+    print_board(board)
+
+
+def computer_move(board, difficulty, player):
+    other_player = int(not bool(player-1))+1
+    move_vals=[]
+    for move in range(COLUMNS):
+        if(valid_move(board, move)):
+            make_move(board, move, player)
+            move_vals.append(minimax(board,
+                                     difficulty, 
+                                     -math.inf, 
+                                     math.inf, 
+                                     other_player, 
+                                     move))
+            unmake_move(board, move)
+        else:
+            if(player==2):
+                move_vals.append(-math.inf)
+            if(player==1):
+                move_vals.append(math.inf)
+
+    min_val = min(move_vals)
+    max_val = max(move_vals)
+    if(player==1):
+        move = move_vals.index(max_val)
+    else:
+        move = move_vals.index(min_val)
+
+    make_move(board, move, player)
+
+    print('Here is the computer\'s move:')
+    print_board(board)
+
+
+
+def play_game():
+    board = np.zeros((ROWS,COLUMNS), dtype=np.intc)
+    player = 1
+    while True:
+        computer_move(board, 6, player)
+        #player_move(board, player)
+        if(check_winning(board,player)):
+            print('Player 1 wins!')
+            break
+        #computer_move(board, 4, player+1)
+        player_move(board, player+1)
+        if(check_winning(board,player+1)):
+            print('Player 2 wins!')
+            break
+
+if __name__=='__main__':
+    play_game()
+    while not prompt_end():
+        play_game()
+
